883526bd7cbda3db1e7d07f38a26b729ce667761
adrianbunea <adrian_bunea28@yahoo.com>
2019-04-22 16:26:37 +0300

    Create class with examples of disinformative names.

---
+++ src/MeaningfulNames/AvoidDisinformation.java
@@ -0,0 +1,17 @@
+package MeaningfulNames;
+import java.util.*;
+
+public class AvoidDisinformation {
+    // DON'T DO THIS
+    String[] accountList; // Not actually a list
+    String XYZControllerForEfficientHandlingOfStrings; // Takes too long to read
+    String XYZControllerForEfficientStorageOfStrings;  // Takes too long to differentiate from the first
+
+    /* Can't tell the difference between the symbols
+    int a = l;
+    if ( O == l )
+        a = O1;
+    else
+        l = 01;
+    */
+}
