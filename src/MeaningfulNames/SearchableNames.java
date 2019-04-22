0ea12bc9af44bb343330070f6eeafdc91071060f
adrianbunea <adrian_bunea28@yahoo.com>
2019-04-22 16:42:24 +0300

    Create class with hard to search names.

---
+++ src/MeaningfulNames/SearchableNames.java
@@ -0,0 +1,12 @@
+package MeaningfulNames;
+
+public class SearchableNames {
+    int s;
+    int[] t;
+
+    private void f() {
+        for (int j=0; j<34; j++) {
+            s += (t[j]*4)/5;
+        }
+    }
+}
