cac76a6848eabd0543061d3bd95e88bc5ea8d4a7
adrianbunea <adrian_bunea28@yahoo.com>
2019-04-22 16:02:31 +0300

    Changed names of variables and constants to give clarity.

--- src/MeaningfulNames/IntentionRevealingNames.java
+++ src/MeaningfulNames/IntentionRevealingNames.java
@@ -7,12 +7,14 @@ public class IntentionRevealingNames {
     int daysSinceModification;
     int fileAgeInDays;
 
-    List<int[]> theList;
-    public List<int[]> getThem() {
-        List<int[]> list1 = new ArrayList<int[]>();
-        for (int[] x : theList)
-            if (x[0] == 4)
-                list1.add(x);
-        return list1;
+    public static final int STATUS_VALUE = 0;
+    public static final int FLAGGED = 4;
+    List<int[]> gameBoard;
+    public List<int[]> getFlaggedCells() {
+        List<int[]> flaggedCells = new ArrayList<int[]>();
+        for (int[] cell : gameBoard)
+            if (cell[STATUS_VALUE] == FLAGGED)
+                flaggedCells.add(cell);
+        return flaggedCells;
     }
 }
