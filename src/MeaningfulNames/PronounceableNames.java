ee29788590106b375987e73d288e1cf13c55a612
adrianbunea <adrian_bunea28@yahoo.com>
2019-04-22 16:33:08 +0300

    Renamed class and properties to be pronounceable.

--- src/MeaningfulNames/PronounceableNames.java
+++ src/MeaningfulNames/PronounceableNames.java
@@ -2,9 +2,9 @@ package MeaningfulNames;
 import java.util.Date;
 
 public class PronounceableNames {
-    class DtaRcrd102 {
-        private Date genymdhms;
-        private Date modymdhms;
-        private final String pszqint = "102";
+    class Customer {
+        private Date generationTimestamp;
+        private Date modificationTimestamp;
+        private final String recordId = "102";
     }
 }
