c2825de261a6fbbd9abe883181db79d188e9a222
adrianbunea <adrian_bunea28@yahoo.com>
2019-04-22 15:56:17 +0300

    Add new function that doesn't reveal intent.

--- src/MeaningfulNames/IntentionRevealingNames.java
+++ src/MeaningfulNames/IntentionRevealingNames.java
@@ -1,8 +1,18 @@
 package MeaningfulNames;
+import java.util.*;
 
 public class IntentionRevealingNames {
     int elapsedTimeInDays;
     int daysSinceCreation;
     int daysSinceModification;
     int fileAgeInDays;
+
+    List<int[]> theList;
+    public List<int[]> getThem() {
+        List<int[]> list1 = new ArrayList<int[]>();
+        for (int[] x : theList)
+            if (x[0] == 4)
+                list1.add(x);
+        return list1;
+    }
 }
