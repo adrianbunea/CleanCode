8f5834cf3fd7b995b7e2bca4e209c99b7cadec7b
adrianbunea <adrian_bunea28@yahoo.com>
2019-04-22 16:31:25 +0300

    Create painful to read class.

---
+++ src/MeaningfulNames/PronounceableNames.java
@@ -0,0 +1,10 @@
+package MeaningfulNames;
+import java.util.Date;
+
+public class PronounceableNames {
+    class DtaRcrd102 {
+        private Date genymdhms;
+        private Date modymdhms;
+        private final String pszqint = "102";
+    }
+}
