7f40e40dc5fca8b6640e980c8104c13fca944ef8
adrianbunea <adrian_bunea28@yahoo.com>
2019-04-23 13:56:07 +0300

    Change names to exclude encoded prefixes and remove other redundant information.

--- src/MeaningfulNames/AvoidEncodings/MemberPrefixes.java
+++ src/MeaningfulNames/AvoidEncodings/MemberPrefixes.java
@@ -2,9 +2,9 @@ package MeaningfulNames.AvoidEncodings;
 
 public class MemberPrefixes {
     public class Part {
-        private String m_dsc;
-        void setName (String name) {
-            m_dsc = name;
+        String description;
+        void setDescription (String description) {
+            this.description = description;
         }
     }
 }
