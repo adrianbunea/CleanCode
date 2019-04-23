5d7e0936cad7115cbb0f48d9ce2647454e57229e
adrianbunea <adrian_bunea28@yahoo.com>
2019-04-23 13:34:46 +0300

    Make names easier to search.

--- src/MeaningfulNames/SearchableNames.java
+++ src/MeaningfulNames/SearchableNames.java
@@ -1,12 +1,17 @@
 package MeaningfulNames;
 
 public class SearchableNames {
-    int s;
-    int[] t;
+    int sum = 0;
+    int[] taskEstimate;
+    int realDaysPerIdealDay = 4;
+    final int WORK_DAYS_PER_WEEK = 5;
+    final int NUMBER_OF_TASKS = 34;
 
-    private void f() {
-        for (int j=0; j<34; j++) {
-            s += (t[j]*4)/5;
+    private void exampleFunction() {
+        for (int j=0; j<NUMBER_OF_TASKS; j++) {
+            int realTaskDays = taskEstimate[j] * realDaysPerIdealDay;
+            int realTaskWeeks = (realTaskDays / WORK_DAYS_PER_WEEK);
+            sum += realTaskWeeks;
         }
     }
 }
