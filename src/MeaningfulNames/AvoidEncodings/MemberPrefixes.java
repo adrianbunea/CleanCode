9e76597b49fedbb71f3fb6610f1ca20ad4329854
adrianbunea <adrian_bunea28@yahoo.com>
2019-04-23 13:53:49 +0300

    Make example of names that contain encodings.

---
+++ src/MeaningfulNames/AvoidEncodings/MemberPrefixes.java
@@ -0,0 +1,10 @@
+package MeaningfulNames.AvoidEncodings;
+
+public class MemberPrefixes {
+    public class Part {
+        private String m_dsc;
+        void setName (String name) {
+            m_dsc = name;
+        }
+    }
+}
