f4379379045ebd5982dfbcba6b0df30c5bcc7d0e
adrianbunea <adrian_bunea28@yahoo.com>
2019-04-26 11:58:25 +0300

    Create a class with pop culture references.

---
+++ src/MeaningfulNames/DontBeCute.java
@@ -0,0 +1,15 @@
+package MeaningfulNames;
+
+public class DontBeCute {
+    void holyHandGrenade() {
+        /* Deletes items */
+    }
+
+    void whack() {
+        /* Kills a process or something */
+    }
+
+    void eatMyShorts() {
+        /* Abort */
+    }
+}
