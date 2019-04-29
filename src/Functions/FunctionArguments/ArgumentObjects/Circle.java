1700e1d13e12598cde3928f5cdc409177abd9d46
adrianbunea <adrian_bunea28@yahoo.com>
2019-04-29 19:46:20 +0300

    Create a class constructor that uses 3 arguments.

---
+++ src/Functions/FunctionArguments/ArgumentObjects/Circle.java
@@ -0,0 +1,11 @@
+package Functions.FunctionArguments.ArgumentObjects;
+
+public class Circle {
+    double x, y, radius;
+
+    public Circle(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+}
