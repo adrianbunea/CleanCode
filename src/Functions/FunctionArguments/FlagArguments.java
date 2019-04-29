bfea2c95bbfe921094f6fc78337e8fc2ee8a6c8b
adrianbunea <adrian_bunea28@yahoo.com>
2019-04-29 19:21:42 +0300

    Create a function that uses a boolean argument to change its behaviour.

---
+++ src/Functions/FunctionArguments/FlagArguments.java
@@ -0,0 +1,13 @@
+package Functions.FunctionArguments;
+
+public class FlagArguments {
+    void render(boolean isSuite) {
+        if (isSuite) {
+            // Do something
+        }
+
+        else {
+            // Do something else
+        }
+    }
+}
