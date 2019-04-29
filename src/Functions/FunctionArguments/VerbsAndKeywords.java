84590d9f5579b7a0e4e4b3ce149cef0651e9b8af
adrianbunea <adrian_bunea28@yahoo.com>
2019-04-29 19:54:22 +0300

    Create functions that could explain their intent and use better.

---
+++ src/Functions/FunctionArguments/VerbsAndKeywords.java
@@ -0,0 +1,8 @@
+package Functions.FunctionArguments;
+
+public class VerbsAndKeywords {
+    void write(String name) {}
+    boolean assertEqual(int expected, int actual) {
+        return expected == actual;
+    }
+}
