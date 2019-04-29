81d811ed63c16355fa91ace7fc62eff1a7379cee
adrianbunea <adrian_bunea28@yahoo.com>
2019-04-29 19:34:56 +0300

    Fix the example to avoid using more arguments than necessary.

--- src/Functions/FunctionArguments/DyadicFunctions/DyadicFunctions.java
+++ src/Functions/FunctionArguments/DyadicFunctions/DyadicFunctions.java
@@ -1,8 +1,8 @@
 package Functions.FunctionArguments.DyadicFunctions;
 
 public class DyadicFunctions {
-    // Bad example of dyadic function
-    void writeField(String field, String name) {
-        // Set field to the value of name
+    String name;
+    void writeField(String name) {
+        this.name = name;
     }
 }
