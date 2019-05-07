130b6ea9b2e490f74d3e9c7759bc21b50cdf4fa9
adrianbunea <adrian_bunea28@yahoo.com>
2019-05-07 11:39:32 +0300

    Create a function that does 2 things
    The function sets the value of a named attribute and checks if it failed
    or not. This is confusing when it is used.

---
+++ src/Functions/CommandQuerySeparation.java
@@ -0,0 +1,15 @@
+package Functions;
+
+public class CommandQuerySeparation {
+    public boolean set(String attribute, String value) {
+        // sets value of named attribute
+        // checks if the operation worked and returns a boolean
+        return true;
+    }
+
+    public void testFunctions() {
+        if (set("name", "Adrian")) {
+            // do something if it worked
+        }
+    }
+}
