ea0b4e5261bbf931a7eefb55da8873aecc66a860
adrianbunea <adrian_bunea28@yahoo.com>
2019-05-10 10:32:38 +0300

    Use whitespace to show operations precedence
    Operations that are evaluated first have no whitespace, while operations
    that do not have precedence use whitespace between terms

---
+++ src/Formatting/HorizontalFormatting/OpennessAndDensity/HelperClasses/OperatorPrecedence/Quadratic.java
@@ -0,0 +1,17 @@
+package Formatting.HorizontalFormatting.OpennessAndDensity.HelperClasses.OperatorPrecedence;
+
+public class Quadratic {
+    public static double root1(double a, double b, double c) {
+        double determinant = determinant(a, b, c);
+        return (-b + Math.sqrt(determinant)) / (2*a);
+    }
+
+    public static double root2(int a, int b, int c) {
+        double determinant = determinant(a, b, c);
+        return (-b - Math.sqrt(determinant)) / (2*a);
+    }
+
+    private static double determinant(double a, double b, double c) {
+        return b*b - 4*a*c;
+    }
+}
